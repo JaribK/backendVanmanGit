778cf5b87540670fb36022eef3be02223fde00d7
JaribK <141174419+JaribK@users.noreply.github.com>
2024-04-09 15:53:05 +0700

    Update settings.py

--- backendCheckIn/settings.py
+++ backendCheckIn/settings.py
@@ -56,10 +56,24 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'backendCheckIn.urls'
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-"http://localhost:5173","https://frontend-vanman-git.vercel.app"
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 ]
+
+CORS_ALLOW_HEADERS = [
+    'Accept',
+    'Accept-Encoding',
+    'Authorization',
+    'Content-Type',
+    'Origin',
+    'X-CSRFToken',
+]
+
 
 TEMPLATES = [
     {
