c1f886c66a965fa0ef1f91e8ad3ddf839cfb393a
JaribK <141174419+JaribK@users.noreply.github.com>
2024-04-25 11:34:21 +0700

    Update urls.py

--- backendCheckIn/urls.py
+++ backendCheckIn/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('',include('models.urls')),
+    path('',include('users.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
