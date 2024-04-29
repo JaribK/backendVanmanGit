ec85a3c4a6ca3296aa5d76b4657fe548013e39b8
JaribK <141174419+JaribK@users.noreply.github.com>
2024-04-29 13:44:02 +0700

    Update settings.py

--- backendCheckIn/settings.py
+++ backendCheckIn/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'django_rest_passwordreset',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
