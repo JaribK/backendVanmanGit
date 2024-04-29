c480f4db77d8591f59cca77170739aa02efecd8d
JaribK <mewn7788@gmail.com>
2024-04-29 10:19:47 +0700

    update

--- backendCheckIn/serializers.py
+++ backendCheckIn/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from users.models import CustomUser
+from users.models import CustomUser, ResetPassword
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
