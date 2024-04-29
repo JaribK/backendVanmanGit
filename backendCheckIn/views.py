9ba9919f98071bca83f1dc22a955155b5ad657d0
JaribK <141174419+JaribK@users.noreply.github.com>
2024-04-29 12:16:50 +0700

    Update views.py

--- backendCheckIn/views.py
+++ backendCheckIn/views.py
@@ -100,15 +100,24 @@ def change_password(request):
             
             # Generate a unique token
             token, _ = Token.objects.get_or_create(user=user)
+            
+            # Generate a unique slug
+            slug = uuid.uuid4().hex  # Using UUID to generate a random slug
+            
+            # Create an instance of ResetPassword model
+            reset_password_instance = ResetPassword.objects.create(
+                email=email,
+                token=token.key,
+                slug=slug
+            )
 
             # Here you pass the context of things above to send them in an email
-            reset_password_url = settings.FRONTEND_URL + '/reset-password/' + token.key
+            reset_password_url = f'{settings.FRONTEND_URL}/reset-password/{slug}'
 
             email_content = f"""
             <html>
             <head>
               <style>
-                /* Add your CSS styles here */
                 body {{
                   font-family: Arial, sans-serif;
                   line-height: 1.6;
@@ -131,10 +140,10 @@ def change_password(request):
               </style>
             </head>
             <body>
-              <div class="container">
+              <div>
                 <h2>Reset Your Password</h2>
                 <p>Please click the following link to reset your password:</p>
-                <p><a class="button" href="{reset_password_url}">Reset Password</a></p>
+                <p><a href="{reset_password_url}">Reset Password</a></p>
                 <p>If you did not request a password reset, please ignore this email.</p>
                 <p>Regards,<br>COMPANY NAME</p>
               </div>
@@ -151,13 +160,8 @@ def change_password(request):
                 fail_silently=False
             )
             
-            # Get the token
-            token = Token.objects.get(user=user)
-            
-            return Response({'message': 'Reset password email sent', 'token': token.key}, status=status.HTTP_200_OK)
+            return Response({'message': 'Reset password email sent'}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == "GET":
         return Response({'message': 'GET request is not supported'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
-
 
