d3fa7a8fb2c901a0b16f407ff2d4e30503ed4d09
JaribK <141174419+JaribK@users.noreply.github.com>
2024-04-29 09:52:19 +0700

    Update models.py

--- users/models.py
+++ users/models.py
@@ -21,3 +21,14 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+class ResetPassword(models.Model):
+    email = models.CharField(max_length=200, null=True)
+    token = models.CharField(max_length=255, null=True)
+    slug = models.SlugField(max_length=255)
+
+    def __str__(self):
+        return self.token
+    # //This thing creates users personalized link, that they visit and have a enter new password view in Front-End.
+    def get_absolute_url(self):
+        return f'/{self.token}/'
