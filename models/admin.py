37d90947116ea516cdfcdca2cc5f2c16fd469ade
JaribK <141174419+JaribK@users.noreply.github.com>
2024-05-02 09:58:51 +0700

    Update admin.py

--- models/admin.py
+++ models/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Timesheets,ConfigSalary,leave_requests,Feedback
+from .models import Timesheets,ConfigSalary,leave_requests,Feedbacks
 
 # Register your models here.
 admin.site.register(Timesheets)
 admin.site.register(ConfigSalary)
 admin.site.register(leave_requests)
-admin.site.register(Feedback)
+admin.site.register(Feedbacks)
